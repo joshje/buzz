b649dd1f6a8af2847b20ca32b6e1d50e1c2d0e03
Josh Emerson <josh@zeebox.com>
2013-12-10 00:21:59 +0000

    Add klout scores

---
+++ klout.js
@@ -0,0 +1,33 @@
+var _ = require('lodash');
+var Klout = require("node_klout");
+
+var klout;
+var config;
+var celebs = {};
+
+var init = function(data){
+    config = data;
+    klout = new Klout(config.klout.api_key, "json", "v2");
+
+     _.each(config.celebs, function(celeb, index){
+        setTimeout(function(){
+            getKloutScore(celeb);
+        }, index * 500);
+    });
+};
+
+var getKloutScore = function(celeb){
+    klout.getKloutIdentity(celeb, function(error, data) {
+        if (! data.id) return;
+        klout.getUserScore(data.id, function(error, data) {
+            if (! data.score) return;
+
+            celebs[celeb] = Math.round(data.score);
+        });
+    });
+};
+
+module.exports = {
+    init: init,
+    celebs: celebs
+};
--- points.js
+++ points.js
@@ -1,21 +1,29 @@
 var _ = require('lodash');
 var twitter = require('./twitter');
+var klout = require('./klout');
 
 var celebPoints = {};
 var config;
 
 var init = function(data){
     config = data;
+
     twitter.init(config);
+    klout.init(config);
     setInterval(calculatePoints, 1000);
 };
 
 var calculatePoints = function(){
+    var celebMentions = twitter.celebMentions;
     var range = Date.now() - config.streaming.range;
-    _.each(twitter.celebMentions, function(mentions, celeb){
-        celebPoints[celeb] = 0;
+    _.each(config.celebs, function(celeb){
+        celebPoints[celeb] = {};
+
+        celebPoints[celeb].twitter = 0;
+        celebPoints[celeb].klout = klout.celebs[celeb];
+        var mentions = celebMentions[celeb];
         _.each(mentions, function(mention){
-            if (mention > range) ++celebPoints[celeb];
+            if (mention > range) ++celebPoints[celeb].twitter;
         });
     });
 };
